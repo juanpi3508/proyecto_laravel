d5deb583b2c2b9526b0186fdf55594d3d37cc02c
Dmms656 <165032342+Dmms656@users.noreply.github.com>
2025-12-28 20:19:15 -0500

    Merge pull request #2
    * Inicio
    
    * Inicio

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/productos', [ProductController::class, 'index'])->name('productos.index');
-Route::get('/productos/{id}', [ProductController::class, 'show'])->name('productos.show');
+Route::get('/', function () {
+    return view('home.index'); // ← tu nueva view
+})->name('home');
+
+Route::get('/productos', [ProductController::class, 'index'])
+    ->name('productos.index');
+
+Route::get('/productos/{id}', [ProductController::class, 'show'])
+    ->name('productos.show');
