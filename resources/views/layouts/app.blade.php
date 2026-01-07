3a5366b94e111d75e546053c6e4a0f1bb456ceec
juanpi3508 <juanpi3508@gmail.com>
2026-01-07 17:58:44 -0500

    * Versionamiento de css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Styles propios -->
-    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}?v={{ filemtime(public_path('assets/css/styles.css')) }}">
 
     @stack('styles')
 </head>
